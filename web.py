afb678405e4b959a7b51717012d97cf566b6af31
gururaghav2925 <gururaghav2925@gmail.com>
2024-03-24 02:50:44 +0530

    Success

--- web.py
+++ web.py
@@ -1,27 +1,5 @@
-# EX01 Developing a Simple Webserver
-## Date:24.03.2024
-
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-```
+from http.server import HTTPServer,BaseHTTPRequestHandler
+content="""
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -250,16 +228,21 @@ Testing the webserver.
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
-```
-
-## OUTPUT:
-
-![alt text](<Screenshot 2024-03-24 023957-1.png>)
-
-![alt text](<Screenshot 2024-03-24 024056-1.png>)
-
-![alt text](<Screenshot 2024-03-24 024102-1.png>)
+"""
 
 
-## RESULT:
-The program for implementing simple webserver is executed successfully.
+
+
+
+
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type','text/html;charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address=('',8000)
+httpd = HTTPServer (server_address,myhandler)
+print("my webser is running....") 
+httpd.serve_forever()       
